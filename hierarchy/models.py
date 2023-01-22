a7d6311248e47b618f43a2a84535f0d11c27bd4b
artemcringe <tyoma.dar@gmail.com>
2023-01-22 21:29:11 +0300

    Added worker model/ made migrations

---
+++ hierarchy/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Workers(models.Model):
+    class Position(models.TextChoices):
+        OPERATOR = "OPER", "Operator"
+        LEADING_SPECIALIST = "LEAD", "Leading Specialist"
+        DIRECTOR_OF_DEPARTMENT = "DIR_DEP", "Director of Department"
+        DEPARTMENT_HEAD = "HEAD_DEP", "Department head"
+        CHIEF_DIRECTOR = "CHIEF", "Chief Director"
+
+    name = models.CharField(max_length=40)
+    surname = models.CharField(max_length=40)
+    patronymic = models.CharField(max_length=40)
+    position = models.CharField(max_length=30,
+                                choices=Position.choices,
+                                default=Position.OPERATOR
+                                )
+    employment_date = models.DateField()
+    salary = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.surname} {self.name} {self.position}"
